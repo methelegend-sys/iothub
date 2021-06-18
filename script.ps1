f4498a74af8da7a1d112586890babc6a9a8b378b
Ankit Kumar <55847022+methelegend-sys@users.noreply.github.com>
2021-06-18 11:11:57 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,3 @@
 az config set extension.use_dynamic_install=yes_without_prompt
 az iot hub device-identity create --hub-name  myiothubdevops --device-id MyPythonDevice
-$env:CONNECTION_STRING = az iot hub device-identity connection-string show --hub-name myiothubdevops --device-id MyPythonDevice --output tsv
-$env:CONNECTION_STRING
-python SimulatedDevice.py
+
