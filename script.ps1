3e9b2dc609f0b46fb21ccce2a3e81efce1ee08ac
Ankit Kumar <55847022+methelegend-sys@users.noreply.github.com>
2021-06-18 11:28:53 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,4 @@
 az config set extension.use_dynamic_install=yes_without_prompt
+$WarningPreference = 'SilentlyContinue'
 az iot hub device-identity create --hub-name  myiothubdevops --device-id MyPythonDevice
 
